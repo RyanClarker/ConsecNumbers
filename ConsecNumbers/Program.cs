37a58b6ff5014aafb49f39c832c80a288fa50502
RyanClarker <clarke.ryan24@gmail.com>
2014-05-13 09:54:17 -0400

    new

---
+++ ConsecNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void FindSum(ref int n)
+        {
+            bool found = false;
+
+                for (int start = 1; start < n; start++)
+                {
+                    int sum = 0;
+
+                    for (int p = start; p < n; p++)
+                    {
+                        sum += p;
+
+                        if (sum == n)
+                        {
+                            found = true;
+                            for (int q = start; q <= p; q++)
+                            {
+                                Console.Write(q + " ");
+                            }
+                            break;
+                        }
+   
+                    }
+
+                }
+
+                if (!found)
+                {
+                    Console.WriteLine("Does not exsist.");
+                }
+            }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FindSum(ref n);
+        }
+    }
+}
